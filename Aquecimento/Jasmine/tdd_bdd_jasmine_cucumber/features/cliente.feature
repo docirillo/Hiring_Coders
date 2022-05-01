c0496a2a0f1b366f3b2bb3e8250a3c3ec30ffe2f
Daniel Cirillo <sapremo@outlook.com>
2022-05-01 16:07:03 -0300

    final aula cucumber I

--- Aquecimento/Jasmine/tdd_bdd_jasmine_cucumber/features/cliente.feature
+++ Aquecimento/Jasmine/tdd_bdd_jasmine_cucumber/features/cliente.feature
@@ -1,4 +1,5 @@
-Scenario: List of clients
-  Given I have {10} clientes in my database
-  When I access the home webpage
-  Then I see the list of {10} itens
+Feature: Clients
+  Scenario: "List of clients"
+    Given I have {10} clientes in my database
+    When I access the home webpage
+    Then I see the list of {10} itens
---
+++ Aquecimento/Jasmine/tdd_bdd_jasmine_cucumber/features/cliente.steps.js
@@ -0,0 +1,14 @@
+const asset = require('assert');
+const { Given, When, Then } = require('@cucumber/cucumber');
+
+Given('I have \\{{int}} clientes in my database', function (qtd) {
+  return 'pending';
+});
+
+When('I access the home webpage', function () {
+  return 'pending';
+});
+
+Then('I see the list of \\{{int}} itens', function (qtd) {
+  return 'pending';
+});
