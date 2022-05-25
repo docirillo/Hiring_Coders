1466b72bf30dc2b1b05d5a75e69f5ae0e6d9cf3f
Daniel Cirillo <daniel.orlandi@outlook.com>
2022-05-25 15:34:09 -0300

    inclusão de botão

--- Semana_3/React/gamagit/src/App.js
+++ Semana_3/React/gamagit/src/App.js
@@ -1,18 +1,14 @@
 import React, { useState } from 'react';
 function App(props) {
-  const [user, setUser] = useState('Cirillo');
   return (
     <>
-      <h1>
-        {props.title} {props.user}
-      </h1>
-      <p>{user}</p>
       <input
         name="user"
         id="user"
         className="userInput"
         placeholder="Usuário"
       />
+      <button type="button">Pesquisar</button>
     </>
   );
 }
