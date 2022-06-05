3ecbea18503b12cd75bfbecc625aad1836aa2de2
Daniel Cirillo <daniel.orlandi@outlook.com>
2022-06-05 12:57:45 -0300

    troca de nome das pastas

--- Semana_3/client-server-example/packages/web/src/pages/Login/index.js
+++ Semana_3/client-server-example/packages/web/src/pages/Login/index.js
@@ -1,29 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch('http://127.0.0.1:8000/authenticate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    })
+      .then((res) => res.json())
+      .then(() => {
+        console.log('success');
+      });
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
   return (
-    <form action="/authenticate" method="POST">
+    <form onSubmit={handleSubmit}>
       <fieldset>
-        <label for="email">E-mail:</label>
+        <label htmlFor="email">E-mail:</label>
         <input
           type="email"
           id="email"
-          mode="email"
-          name="email"
-          autocomplete="username"
+          inputMode="email"
+          value={email}
+          onChange={handleEmailChange}
+          autoComplete="username"
         />
       </fieldset>
 
       <fieldset>
-        <label for="password">Password:</label>
+        <label htmlFor="password">Password:</label>
         <input
           type="password"
           id="password"
-          name="password"
-          autocomplete="current-password"
+          value={password}
+          onChange={handlePasswordChange}
+          autoComplete="current-password"
         />
       </fieldset>
-      <button type="submit">Login</button>
+      <button onClick={handleSubmit} type="submit">
+        Login
+      </button>
     </form>
   );
 }
