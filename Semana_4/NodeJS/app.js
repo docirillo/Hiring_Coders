1502884a16a5efda2cea368f8a7ca06224a414d3
Daniel Cirillo <daniel.orlandi@outlook.com>
2022-06-05 21:10:37 -0300

    npm básico

--- Semana_4/NodeJS/app.js
+++ Semana_4/NodeJS/app.js
@@ -1,14 +1,35 @@
+// Incluindo as bibliotecas
 const http = require('http');
+const url = require('url');
+const queryString = require('query-string');
 
-const hostname = '127.0.0.1';
+//Definição de endereço / URL
+const hostname = '127.0.0.1'; //localhost
 const port = 3000;
 
+// Implementação da regra de negócio
 const server = http.createServer((req, res) => {
+  //Pegar a pergunta na url
+  const params = queryString.parse(url.parse(req.url, true).search);
+
+  // Verificar a pergunta e escolher uma resposta
+  let resposta = '';
+
+  if (params.pergunta == 'melhor-filme') {
+    resposta = 'star wars';
+  } else if (params.pergunta == 'melhor-tecnologia-backend') {
+    resposta = 'NodeJS';
+  } else {
+    resposta = 'nao sei, desculpe :~(';
+  }
+
+  // Retornar a resposta escolhida
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end(resposta);
 });
 
+//Execução do servidor
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
