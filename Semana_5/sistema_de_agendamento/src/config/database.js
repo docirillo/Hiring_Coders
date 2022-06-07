27b383eeaa27ad8fadf1d4bcb5886c215a035b80
Daniel Cirillo <daniel.orlandi@outlook.com>
2022-06-07 14:25:46 -0300

    ORM

---
+++ Semana_5/sistema_de_agendamento/src/config/database.js
@@ -0,0 +1,8 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'docker',
+  database: 'sistema',
+  define: { timestamps: true, underscored: true, underscoredAll: true },
+};
