8c320ffaeef3c3b7965293129c589436b9a82208
Daniel Cirillo <daniel.orlandi@outlook.com>
2022-06-07 11:35:41 -0300

    nodemon e sucrase configurados

--- Semana_5/sistema_de_agendamento/src/server.js
+++ Semana_5/sistema_de_agendamento/src/server.js
@@ -1,4 +1,5 @@
-const app = require('./app');
+import app from './app';
 
 //portas do front e do back devem ser diferentes
 app.listen(3333);
+console.log('Server initialized successfully on port 3333');
